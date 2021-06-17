a43c8f732090f5499183ba3b02c06d944317dc4a
McIntosh, Claire <clairemcintosh88@gmail.com>
2021-06-17 16:12:18 +0100

    changes to layout and css

--- interactive-price-app/config/environment.js
+++ interactive-price-app/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'interactive-price-app',
     environment,
-    rootURL: '/',
+    rootURL: '/interactive-price-app',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
