93f4fb8c43ef46d0c28e9dad6302c3a116222e78
McIntosh, Claire <claire.mcintosh@codeworldwide.com>
2021-09-21 11:47:39 +0100

    more work

--- interactive-price-app/app/controllers/application.js
+++ interactive-price-app/app/controllers/application.js
@@ -17,4 +17,18 @@ export default class ApplicationController extends Controller {
         console.log(millionPageViews);
     }
 
+    steps(){
+
+        let stepSlider = document.getElementById('slider-step');
+
+        noUiSlider.create(stepSlider, {
+            start: [20, 80],
+            step: 10,
+            range: {
+                'min': 0,
+                'max': 100
+            }
+            
+        });
+    }
 }
--- interactive-price-app/app/routes/application.js
+++ interactive-price-app/app/routes/application.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+
 
 export default class ApplicationRoute extends Route {
+model() {
+    console.log('test');
 }
+
+};
